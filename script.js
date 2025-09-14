9febabefa7275a9e110afffa0f724e24c46fc23e
milson94 <mangueleedmilson@gmail.com>
2025-09-14 10:09:06 +0200

    enhanced the resume

--- script.js
+++ script.js
@@ -1,26 +1,50 @@
-// script.js
-document.getElementById('en-btn').addEventListener('click', function() {
-    showLanguage('en');
-});
+document.addEventListener('DOMContentLoaded', () => {
+    const ptBtn = document.getElementById('pt-btn');
+    const enBtn = document.getElementById('en-btn');
+    const darkModeBtn = document.getElementById('dark-mode-btn');
 
-document.getElementById('pt-btn').addEventListener('click', function() {
-    showLanguage('pt');
-});
+    const elementsToToggle = [
+        'h1', 'p', 'h2', 'h3', 'h4', 'ul', 'li', 'span', 'a'
+    ];
+    
+    // Set initial language to English
+    const setLanguage = (lang) => {
+        const langElements = document.querySelectorAll(`[class$="${lang}"]`);
+        const otherLang = lang === 'en' ? 'pt' : 'en';
+        const otherLangElements = document.querySelectorAll(`[class$="${otherLang}"]`);
+        
+        langElements.forEach(el => el.style.display = 'block');
+        otherLangElements.forEach(el => el.style.display = 'none');
+    };
 
-document.getElementById('dark-mode-btn').addEventListener('click', function() {
-    document.body.classList.toggle('dark-mode');
-});
+    enBtn.addEventListener('click', () => setLanguage('en'));
+    ptBtn.addEventListener('click', () => setLanguage('pt'));
 
-function showLanguage(lang) {
-    const elements = document.querySelectorAll(`.${lang}`);
-    const otherLang = lang === 'en' ? 'pt' : 'en';
-    const otherElements = document.querySelectorAll(`.${otherLang}`);
+    // Set initial dark mode state
+    const body = document.body;
+    const isDarkMode = body.classList.contains('dark-mode');
+    
+    // Set initial icon based on the default state
+    const icon = darkModeBtn.querySelector('i');
+    if (isDarkMode) {
+        icon.classList.remove('fa-moon');
+        icon.classList.add('fa-sun');
+    } else {
+        icon.classList.remove('fa-sun');
+        icon.classList.add('fa-moon');
+    }
 
-    elements.forEach(el => {
-        el.style.display = 'block';
+    darkModeBtn.addEventListener('click', () => {
+        body.classList.toggle('dark-mode');
+        if (body.classList.contains('dark-mode')) {
+            icon.classList.remove('fa-moon');
+            icon.classList.add('fa-sun');
+        } else {
+            icon.classList.remove('fa-sun');
+            icon.classList.add('fa-moon');
+        }
     });
 
-    otherElements.forEach(el => {
-        el.style.display = 'none';
-    });
-}
+    // Initial load language
+    setLanguage('en');
+});
